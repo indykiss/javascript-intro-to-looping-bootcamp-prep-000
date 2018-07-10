bcd7fa260f70b9ac3b975877f3d3127975dd90ea
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-10 08:38:14 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+
+
+function forLoop (arr) {
+
+  for (var i = 0; i < 25 ; i++) {
+    if(var i === 1) {
+    return "I am 1 strange loop.";
+    }
+    else {
+      return "I am a " + i + " strange loops." ;
+    }
+  }
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(--num)
+    }
+    console.log('done');
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop (arr) {
+  do {
+    [
+      array.length > 0 && maybeTrue()
+    ];
+  } while ([maybeTrue]);
+  return;
+}
